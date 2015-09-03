736c1b41fa7a4352be0c892d166697a994000b41
Xavi G <alexandro.x.guerrero@gmail.com>
2015-09-02 19:27:44 -0500

    practicing git and cleaning up files

--- movies.rb
+++ movies.rb
@@ -40,40 +40,16 @@ leo_movies = movies.select { |movie| movie[:stars].include? "Leonardo DiCaprio" 
 # alternately:
 # leo_movies = movies.select { |movie| movie.fetch(:stars).include? "Leonardo DiCaprio" }
 
+# print cheaper_movies
+# print leo_movies
+
 puts "\n\nThese movies had a budget of less than $100M:\n "
 
-#print cheaper_movies
 cheaper_movies.each { |movie| puts movie.fetch(:title) + "\n " }
 
 puts "------------------"
 puts "\nThese movies starred Leonardo DiCaprio:\n "
 
-#print leo_movies
 leo_movies.each { |movie| puts movie.fetch(:title) + "\n " }
 
-#movies.each { |movie| print movie.fetch(:stars) }#+ "\n" }
-
-# def say_hi(person)
-#   puts "hiiiiiii....#{person}"
-# end
-#
-# puts "\n\n-----------"
-# puts "A simple method: for say_hi"
-# say_hi("Rufus")
-#
-# class Person
-#   def initialize(person_name, person_age)
-#     @name = person_name
-#     @age = person_age
-#   end
-#
-#
-#   def greet
-#     puts "#{@name}, who is #{@age} says......'ey fuckface'"
-#   end
-# end
-#
-# puts "\n\n-----------"
-# puts "getting classy: for Person class"
-# some_guy = Person.new("Travo",31)
-# some_guy.greet
+# cheap_leo_movies = movies.select { |movie| movie[:budget] < 100 && movie[:stars]}
